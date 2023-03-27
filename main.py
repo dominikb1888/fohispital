5f5ce689232774a7524e0b985c43f0cc6ca22d7e
Dominik Boehler <dominik.boehler@gmx.net>
2023-03-27 11:50:26 +0200

    update after second  session

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import os
 import json
+import sys
 
 app = FastAPI()
 
@@ -16,3 +17,29 @@ async def root():
             patients.append(json.load(file))
 
     return patients
+
+@app.post("/patients/create")
+async def patient_create(jsonData):
+    # How to secure our open "post box"?
+    # Check for valid json
+    print(jsonData, file=sys.stderr)
+    try:
+        patient = json.load(jsonData)
+    except ValueError as err:
+        return False
+
+    # TODO: Check header for the right Content-Type: application/json+fhir
+
+    file_id = len(os.listdir('fhir_resources')) + 1
+    filename = f"patient_{file_id}.json"
+
+    with open(filename, 'w') as file:
+        json.dump(patient, file)
+
+    return 'OK'
+
+
+# Agenda:
+
+# 1. Play with the data locally. Access it, use it, filter it, display
+# 2. Adapt our server to send and accept valid JSON FHIR data
